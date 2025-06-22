79a954a8c0c8ced3ae7cb567849fe0548d6d643b
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-06-22 15:10:31 +0900

    パンくずリスト修正

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -18,7 +18,7 @@
             <h2>{{ $groupName }}</h2>
             <ul class="link-grid">
                 @foreach ($pages as $page)
-                    <li><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li>
+                    <li class="top-link"><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li>
                 @endforeach
             </ul>
         </section>
