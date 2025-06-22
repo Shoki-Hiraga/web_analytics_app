ba0dd2fab0725579860b2daed90888f9f282ebd1
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-06-22 14:23:11 +0900

    リンク表示整理

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,15 @@
-    <ul>
-        @php
-            use App\Helpers\BreadcrumbHelper;
-            $pages = BreadcrumbHelper::getLinks();
-        @endphp
+    @php
+        use App\Helpers\BreadcrumbHelper;
+        $groups = BreadcrumbHelper::getLinks();
+    @endphp
 
-        @foreach ($pages as $page)
-            <li><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li>
-        @endforeach
-    </ul>
+    @foreach ($groups as $groupName => $pages)
+        <section class="group">
+            <h2>{{ $groupName }}</h2>
+            <ul class="link-grid">
+                @foreach ($pages as $page)
+                    <li><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li>
+                @endforeach
+            </ul>
+        </section>
+    @endforeach
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -8,15 +8,20 @@
 
 <body>
     <h1>@include('components.sitename') ページ一覧</h1>
-    <ul>
-        @php
-            use App\Helpers\BreadcrumbHelper;
-            $pages = BreadcrumbHelper::getLinks();
-        @endphp
+    @php
+        use App\Helpers\BreadcrumbHelper;
+        $groups = BreadcrumbHelper::getLinks();
+    @endphp
 
-        @foreach ($pages as $page)
-            <h2><li><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li></h2>
-        @endforeach
-    </ul>
+    @foreach ($groups as $groupName => $pages)
+        <section class="group">
+            <h2>{{ $groupName }}</h2>
+            <ul class="link-grid">
+                @foreach ($pages as $page)
+                    <li><a href="{{ $page['url'] }}">{{ $page['name'] }}</a></li>
+                @endforeach
+            </ul>
+        </section>
+    @endforeach
 </body>
 </html>
